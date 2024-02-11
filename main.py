166c6fc71f7292b9d1d7f3922c85994a858c9e3c
OlikhovKirill <olikhovkirill@gmail.com>
2024-02-11 14:46:15 +0300

    keyboard choose added

--- main.py
+++ main.py
@@ -1,42 +1,20 @@
-from modules.wallet_create import WalletCreate
-from modules.get_addresses import GetAddresses
+from utils import GetAddresses, WalletCreate, choose_mode
 from termcolor import colored
 
 
 def main():
-    print(colored('This program will help you with managing your EVM accounts.\n'
-                                       'Please enter on of the following options:\n'
-                                       '>>>1 - Create a batch of EVM accounts\n'
-                                       '>>>2 - Get public addresses from private keys\n', 'light_cyan', 'on_black'))
-    while True:
-        try:
-            option = int(input())
-            if option == 1:
-                WalletCreate().create_evm_account()
-                break
-            elif option == 2:
-                GetAddresses().addr_from_pks()
-                break
-            # elif option == 3:
-            #     GetAddresses().addr_from_mnemonic()
-            #     break
-            else:
-                print(colored("\nInvalid number. Enter a number from the list.\n", 'red', 'on_black'))
-        except ValueError:
-            print(colored("\nInvalid input. Enter a number.\n", 'red', 'on_black'))
+    choice = choose_mode()
+    if choice == 'Create a': #kostil
+        WalletCreate().create_evm_account()
+    else:
+        GetAddresses().addr_from_pks()
 
 
 if __name__ == '__main__':
     print(colored("""
-                                                                                                                                                                 
- **       **     **     **       **       ******** **********              ****     ****     **     ****     **     **       ********  ******** *******  
-/**      /**    ****   /**      /**      /**///// /////**///              /**/**   **/**    ****   /**/**   /**    ****     **//////**/**///// /**////** 
-/**   *  /**   **//**  /**      /**      /**          /**                 /**//** ** /**   **//**  /**//**  /**   **//**   **      // /**      /**   /** 
-/**  *** /**  **  //** /**      /**      /*******     /**                 /** //***  /**  **  //** /** //** /**  **  //** /**         /******* /*******  
-/** **/**/** **********/**      /**      /**////      /**                 /**  //*   /** **********/**  //**/** **********/**    *****/**////  /**///**  
-/**** //****/**//////**/**      /**      /**          /**                 /**   /    /**/**//////**/**   //****/**//////**//**  ////**/**      /**  //** 
-/**/   ///**/**     /**/********/********/********    /**                 /**        /**/**     /**/**    //***/**     /** //******** /********/**   //**
-//       // //      // //////// //////// ////////     //                  //         // //      // //      /// //      //   ////////  //////// //     //   \n                                                                                                                                                            
-""", 'red', 'on_black', ['bold']))
-
+ _   _   _   ______   _        _        ______ _______    _________   ______   ______   ______   ______   ______  ______  
+| | | | | | | |  | | | |      | |      | |       | |     | | | | | \ | |  | | | |  \ \ | |  | | | | ____ | |     | |  | \ 
+| | | | | | | |__| | | |   _  | |   _  | |----   | |     | | | | | | | |__| | | |  | | | |__| | | |  | | | |---- | |__| | 
+|_|_|_|_|_/ |_|  |_| |_|__|_| |_|__|_| |_|____   |_|     |_| |_| |_| |_|  |_| |_|  |_| |_|  |_| |_|__|_| |_|____ |_|  \_\ \n """, 'red'))
     main()
+    input(' > Press any button to close')
--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ from datetime import datetime
 from eth_account import Account
 from mnemonic import Mnemonic
 from termcolor import colored
+import sys
+sys.__stdout__ = sys.stdout #solve error with inquirer import
+from inquirer import prompt, List
 
 
 class WalletCreate:
@@ -10,32 +13,32 @@ class WalletCreate:
     def set_qty():
         while True:
             try:
-                qty = int(input(colored('\nHow many accounts need to be created: ', 'light_cyan', 'on_black')))
+                qty = int(input(colored('\nHow many accounts need to be created: ', 'light_cyan')))
                 return qty
             except ValueError:
-                print(colored("\nInvalid input. Enter a number.", 'red', 'on_black'))
+                print(colored("\nInvalid input. Enter a number.", 'red'))
 
     @staticmethod
     def set_amount():
         while True:
             try:
-                amnt = int(input(colored('\nHow many "address-private key" pairs will be created under each seed phrase (Enter 1 by default): ', 'light_cyan', 'on_black')))
+                amnt = int(input(colored('\nHow many "address-private key" pairs will be created under each seed phrase (Enter 1 by default): ', 'light_cyan')))
                 return amnt
             except ValueError:
-                print(colored("\nInvalid input. Enter a number.", 'red', 'on_black'))
+                print(colored("\nInvalid input. Enter a number.", 'red'))
 
     @staticmethod
     def set_seed_strength():
         strength_dict = {12: 128, 15: 160, 18: 192, 21: 224, 24: 256}
         while True:
             try:
-                length = int(input(colored('\nHow many words would make up a seed phrase - 12/15/18/21/24: ','light_cyan', 'on_black')))
+                length = int(input(colored('\nHow many words would make up a seed phrase - 12/15/18/21/24: ','light_cyan')))
                 if length in strength_dict:
                     return strength_dict[length]
                 else:
-                    print(colored("\nInvalid length of seed phrase. Please choose from the following options 12, 15, 18, 21 or 24.", 'red', 'on_black'))
+                    print(colored("\nInvalid length of seed phrase. Please choose from the following options 12, 15, 18, 21 or 24.", 'red'))
             except ValueError:
-                print(colored("\nInvalid input. Please enter a number.", 'red', 'on_black'))
+                print(colored("\nInvalid input. Please enter a number.", 'red'))
 
     @staticmethod
     def determine_date_time():
@@ -43,7 +46,7 @@ class WalletCreate:
         return now
 
     def create_evm_account(self):
-        print(colored('\nThe accounts will be saved in a csv file in the following format "Seed-phrase | Address | Private Key"', 'light_cyan', 'on_black', ['bold']))
+        print(colored('\nThe accounts will be saved in a csv file in the following format "Seed-phrase | Address | Private Key"', 'light_cyan'))
 
         qty = self.set_qty()
         amnt = self.set_amount()
@@ -78,4 +81,33 @@ class WalletCreate:
                     writer.writerow(row)
 
         print(colored(f'\n{qty} seed-phrases with {amnt} "address-private key" pairs were created\n'
-              f'A total of {qty*amnt} wallets were created', 'light_cyan', 'on_black'))
+              f'A total of {qty*amnt} wallets were created', 'green'))
+
+
+class GetAddresses:
+
+    @staticmethod
+    def addr_from_pks():
+        with open('data/pks.txt', 'r') as f:
+            pks = [line.strip() for line in f]
+            with open('result/addresses.txt', 'w') as f:
+                for pk in pks:
+                    if len(pk) < 64:
+                        print(colored('Invalid private key', 'red'))
+                    else:
+                        address = Account.from_key(pk).address
+                        f.write(address + '\n')
+            print(colored('All valid addresses were successfully imported to result/addresses.txt', 'green'))
+
+
+def choose_mode():
+    questions = [
+        List('prefered_path', message="Please choose one from the following options",
+             choices=[
+                'Create a batch of EVM accounts',
+                'Get public addresses from private keys',
+             ]
+        )
+    ]
+    path = prompt(questions)['prefered_path']
+    return ' '.join(path.split()[:2])
